79b0b62f60334030ad7f48408e51c0717044fc04
Chinchinberry <jonjon0424@gmail.com>
2020-09-12 21:44:47 -0700

    init commit

---
+++ ramenreview/src/main/java/com/example/ramenreview/RamenreviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.ramenreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamenreviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RamenreviewApplication.class, args);
+	}
+
+}
---
+++ ramenreview/src/test/java/com/example/ramenreview/RamenreviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ramenreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RamenreviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
